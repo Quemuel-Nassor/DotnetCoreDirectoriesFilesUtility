b60a5c25c8bf8fbe6254ab714bce0d8e33f01920
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2023-01-22 12:39:11 -0300

    added functionalities

---
+++ FilesFoldersUtility/Src/IUtility.cs
@@ -0,0 +1,8 @@
+﻿namespace FilesFoldersUtility.Src
+{
+    public interface IUtility
+    {
+        string GetUrl(string folderName, string filename = null);
+        string GetPath(string folderName, string filename = null);
+    }
+}
