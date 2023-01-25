fb83462565775520b4c67b21f1ac8fb426cb5ebb
Quemuel Alves Nassor <quemuel.nassor@novasingular.com.br>
2023-01-24 23:20:41 -0300

    added register helper

---
+++ FilesFoldersUtility/DirectoriesFilesUtilityExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+
+namespace FilesFoldersUtility
+{
+    public static class DirectoriesFilesUtilityExtensions
+    {
+        public static IServiceCollection RegisterDirectoriesFilesUtility(this IServiceCollection services, Action<DirectoriesFilesUtilityOptions> folders)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+            if (folders == null)
+            {
+                throw new ArgumentNullException(nameof(folders));
+            }
+
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.Configure(folders);
+            services.TryAddSingleton<IDirectoriesFilesUtility, DirectoriesFilesUtility>();
+            return services;
+        }
+    }
+}
