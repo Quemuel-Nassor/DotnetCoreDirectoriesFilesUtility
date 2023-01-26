f04036b4cedb88a7f48086b7ecadbc108d5a085d
Quemuel Alves Nassor <quemuel.nassor@novasingular.com.br>
2023-01-26 08:46:25 -0300

    implemented sanitizer for input data

---
+++ DotnetCoreDirectoriesFilesUtility/SanitizerHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace DotnetCoreDirectoriesFilesUtility
+{
+    internal static class SanitizerHelper
+    {
+        private static TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+        public static string Sanitize(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return name;
+
+            name = Regex.Replace(name, @"([\\\/|<>*:“?])", "", RegexOptions.Compiled);
+            name = textInfo.ToTitleCase(name).Trim();
+
+            return Regex.Replace(name, @"\s+", "-", RegexOptions.Compiled);
+        }
+    }
+}
