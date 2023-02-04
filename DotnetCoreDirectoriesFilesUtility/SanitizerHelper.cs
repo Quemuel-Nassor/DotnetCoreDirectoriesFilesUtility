283da858e342a5a0037e0f21a46849a312b679d1
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2023-02-04 13:43:20 -0300

    upgraded sanitization tool

--- DotnetCoreDirectoriesFilesUtility/SanitizerHelper.cs
+++ DotnetCoreDirectoriesFilesUtility/SanitizerHelper.cs
@@ -1,4 +1,7 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -9,18 +12,18 @@ namespace DotnetCoreDirectoriesFilesUtility
     {
         private static TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
 
-        public static string Sanitize(string name)
+        public static string SanitizeDir(this string name)
         {
             if (string.IsNullOrWhiteSpace(name))
                 return name;
 
             name = Regex.Replace(name, @"([\\\/|<>*:“?])", "", RegexOptions.Compiled);
-            name = RemoveAccent(textInfo.ToTitleCase(name).Trim());
+            name = textInfo.ToTitleCase(name).Trim().RemoveAccent();
 
             return Regex.Replace(name, @"\s+", "-", RegexOptions.Compiled);
         }
 
-        public static string RemoveAccent(string name)
+        public static string RemoveAccent(this string name)
         {
             if (string.IsNullOrWhiteSpace(name))
                 return name;
@@ -29,6 +32,37 @@ namespace DotnetCoreDirectoriesFilesUtility
             .Normalize(NormalizationForm.FormD)
             .Where(ch => char.GetUnicodeCategory(ch) != UnicodeCategory.NonSpacingMark)
             .ToArray());
+        }
+    }
+
+    internal static class FileHelper
+    {
+        public static string AutoIndexFile(this string destinationPath)
+        {
+            if (!File.Exists(destinationPath))
+                return destinationPath;
+
+            string filePath = Path.GetDirectoryName(destinationPath);
+            string[] files = Directory.GetFiles(filePath);
+
+            var filesThatContainsSameName = files.Where(f => f.GetFileName().IndexOf(destinationPath.GetFileName(), StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+            filesThatContainsSameName.Sort();
+
+            List<int> filesIndexes = filesThatContainsSameName
+                .Select(f => Regex.Match(f, @"\(\d*\)").Value.Replace("(", "").Replace(")", ""))
+                .Where(f => !string.IsNullOrWhiteSpace(f))
+                .Select(f => Convert.ToInt32(f))
+                .ToList();
+
+            int index = (filesIndexes.Any() ? filesIndexes.Max() : 0) + 1;
+
+            return destinationPath.Replace(".", $" ({index}).");
+
+        }
+
+        private static string GetFileName(this string filePath)
+        {
+            return Path.GetFileNameWithoutExtension(filePath);
         }
     }
 }
